76a641211cb7078362bc888c97e540af783ff0c9
HNAHP <163244532+HNAHP@users.noreply.github.com>
2024-03-13 15:05:26 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main() {
     printf("Enter the height and width of the rectangle: ");
     scanf("%d %d", &height, &width);
     
-    //Write your code here
-   
+    perimeter = ( height + width ) * 2;
+    
     printf("Perimeter of the rectangle: %d\n", perimeter);
 
     return 0;
